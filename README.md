6851aebcc22c65e540fefd3dde27e6cdcdd7bc8e
DominicDev <s0l0estudi4r@gmail.com>
2025-05-01 16:12:48 +0200

    v0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-![n1](preview/n1.avif)
-![n2](preview/n2.avif)
+[![Foto Preview](preview/n97.avif)](https://20essentials.github.io/project-000-097)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-000-096" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-000-098" target="_blank">&#8594;</a>
+</div>
--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-console.group("Reference");
-console.log(
-  "Vector Background: ",
-  "https://www.vecteezy.com/vector-art/34614990-black-and-white-geometric-pattern-abstract-geometric-seamless-pattern-black-and-white-squares-mosaic-modern-stylish-texture-monochrome-trellis-wallpaper-backdrop-wrapping-vector-illustration"
-);
-console.groupEnd();
-
 const d = document;
-const $ = (el) => d.querySelector(el);
+const $ = el => d.querySelector(el);
 
 /*************************** CARD ANIMATION ***************************/
 
-const { width, height } = $(".am-card").getBoundingClientRect();
+const { width, height } = $('.am-card').getBoundingClientRect();
 const halfWidth = width / 2;
 const halfHeight = height / 2;
 
 function mover(e) {
-  if (e.target.matches(".am-card")) {
+  if (e.target.matches('.am-card')) {
     let { offsetX, offsetY } = e;
 
     let rotationX = ((offsetX - halfWidth) / halfWidth) * 10;
@@ -28,9 +21,9 @@ function mover(e) {
   }
 }
 
-d.addEventListener("mousemove", mover);
-d.addEventListener("touchstart", (e) => {
-  if (e.target.matches(".am-card")) {
-    d.removeEventListener("mousemove", mover);
+d.addEventListener('mousemove', mover);
+d.addEventListener('touchstart', e => {
+  if (e.target.matches('.am-card')) {
+    d.removeEventListener('mousemove', mover);
   }
 });
